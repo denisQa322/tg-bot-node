1e29bc6a52c29f5510c3bc138ef3ee6cb8256418
denisQa322 <denis.isakov.99@gmail.com>
2024-12-10 18:11:09 +0500

    small fix

--- bot.js
+++ bot.js
@@ -1,8 +1,71 @@
 const { Telegraf, Markup } = require('telegraf');
 const path = require('path');
+const fs = require('fs');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_API_KEY);
+
+// Файл для записи данных
+const logFilePath = path.join(__dirname, 'user_interactions.json');
+
+// Проверка, существует ли файл, если нет, создаем его с пустым массивом
+if (!fs.existsSync(logFilePath)) {
+  try {
+    fs.writeFileSync(logFilePath, JSON.stringify([])); // Записываем пустой массив в файл
+  } catch (error) {
+    console.error('Ошибка при создании файла:', error);
+  }
+}
+
+// Функция для чтения данных из JSON-файла
+function readLogs() {
+  try {
+    const data = fs.readFileSync(logFilePath, 'utf-8');
+    if (!data.trim()) {
+      return []; // Возвращаем пустой массив, если данные пустые
+    }
+    return JSON.parse(data);
+  } catch (error) {
+    // Если файл пуст или поврежден, возвращаем пустой массив
+    console.error('Ошибка при чтении файла:', error);
+    return [];
+  }
+}
+
+// Функция для записи данных в JSON-файл
+function writeLogs(logs) {
+  try {
+    fs.writeFileSync(logFilePath, JSON.stringify(logs, null, 2));
+  } catch (error) {
+    console.error('Ошибка при записи в файл:', error);
+  }
+}
+
+// Функция для логирования действий пользователя
+function logUserAction(userId, action, message) {
+  const logs = readLogs();
+  const timestamp = new Date().toISOString();
+
+  let userLog = logs.find((log) => log.userId === userId);
+
+  if (!userLog) {
+    userLog = {
+      userId,
+      firstInteraction: timestamp,
+      actions: [],
+    };
+    logs.push(userLog);
+  }
+
+  userLog.actions.push({
+    action,
+    timestamp,
+    message,
+  });
+
+  // Записываем обновленные данные обратно в файл
+  writeLogs(logs);
+}
 
 // Объект для хранения времени последнего взаимодействия пользователей
 const userLastInteraction = {};
@@ -21,7 +84,7 @@ const messages = {
   pvzInfo:
     "Вы можете узнать информацию о ПВЗ нажав на иконку магазина справа от города, под Вашим ID, при авторизации на сайте с компьютера (1ый скриншот).\n\nС телефона для просмотра информации о ПВЗ Вы можете нажать на иконку меню слева вверху, затем нажать на 'Ваш пункт выдачи заказов' (2ой и 3ий скриншот).",
   statusInfo:
-    'Чтобы получить информацию о текущем статусе Вашего заказа при авторизации с компьютера вам нужно войти в историю в отслеживании, нажать на "i" в начале строки с заказом затем нажать на "Промежуточные статусы заказа", так вы сможете узнать текущий статус вашего заказа.\n\n При авторизации с телефона вам нужно войти во вкладку "Заказы" (вторая слева внизу) и войти в карточку с заказом, далее нужно нажать на "Путь заказа" - последний указанный статус является актуальным для Вашего заказа',
+    'Чтобы получить информацию о текущем статусе Вашего заказа при авторизации с компьютера вам нужно войти в историю в отслеживании, нажать на "i" в начале строки с заказом, таким образом вы сможете узнать текущий статус вашего заказа.\n\n При авторизации с телефона вам нужно войти во вкладку "Заказы" (вторая слева внизу) и войти в карточку с заказом, далее нужно нажать на "Путь заказа" - последний указанный статус является актуальным для Вашего заказа',
   stockInfo:
     'У нас не имеется запчастей в наличии, так как мы интернет-магазин, Вы можете самостоятельно проверить наличие на сайте и оформить заказ.\n\nДля этого Вам нужно:\n1. Найти интересующую Вас деталь по артикулу, либо по схеме добавив Ваше ТС (транспортное средство) по VIN или FRAME коду\n2.Добавить нужную Вам деталь в корзину.\n3.Пополнить Ваш баланс на сайте одним из вариантов оплаты.\n4.Выбрать нужный вам пункт выдачи\n5.Оформить заказ и наблюдать за его статусом.',
   returnInfo:
@@ -47,7 +110,7 @@ function questionsKeyboard(questions) {
 
 // Функция создания клавиатуры с кнопками остались ли вопросы и возвратом в начало
 function ussualKeyboard() {
-  return Markup.keyboard([['Остались вопросы', 'Вернуться в начало']])
+  return Markup.keyboard([['Остались вопросы', 'Вернуться в главное меню']])
     .resize()
     .oneTime();
 }
@@ -92,6 +155,8 @@ bot.start(async (ctx) => {
   try {
     const userId = ctx.from.id;
 
+    logUserAction(userId, 'start');
+
     // Проверяем, прошло ли больше 24 часов
     if (!hasOneDayPassed(userLastInteraction[userId])) {
       ctx.reply('Чем мы можем помочь?');
@@ -115,11 +180,15 @@ bot.start(async (ctx) => {
 // Обработчик выбора "Розничный клиент"
 bot.hears(['Розничный клиент'], async (ctx) => {
   try {
+    const userId = ctx.from.id;
+
+    logUserAction(userId, 'Розничный клиент');
+
     const retailQuestions = questionsKeyboard([
       ['Как оформить заказ?', 'Как пополнить баланс?'],
       ['Как узнать какой выбран ПВЗ?', 'Как узнать текущий статус заказа?'],
       ['Есть ли запчасть в наличии?', 'Как оформить возврат запчасти?'],
-      ['Вопроса нет в предложенных', 'Вернуться назад'],
+      ['Вопроса нет в предложенных', 'Вернуться в главное меню'],
     ]);
     ctx.reply(messages.client, retailQuestions);
   } catch (error) {
@@ -130,10 +199,13 @@ bot.hears(['Розничный клиент'], async (ctx) => {
 // Обработчик выбора "Оптовый клиент"
 bot.hears(['Оптовый клиент'], async (ctx) => {
   try {
+    const userId = ctx.from.id;
+
+    logUserAction(userId, 'Оптовый клиент');
     const wholeQuestions = questionsKeyboard([
       ['Как оформить заказ?', 'Как пополнить баланс?'],
       ['Как узнать текущий статус заказа?', 'Как оформить возврат запчасти?'],
-      ['Вопроса нет в предложенных', 'Вернуться назад'],
+      ['Вопроса нет в предложенных', 'Вернуться в главное меню'],
     ]);
     ctx.reply(messages.client, wholeQuestions);
   } catch (error) {
@@ -144,7 +216,10 @@ bot.hears(['Оптовый клиент'], async (ctx) => {
 // Обработчик выбора "Клиент EMEX.RU"
 bot.hears(['Клиент EMEX.RU'], async (ctx) => {
   try {
-    ctx.reply(messages.emex);
+    const userId = ctx.from.id;
+
+    logUserAction(userId, 'Клиент EMEX.RU');
+    ctx.reply(messages.emex, questionsKeyboard([['Вернуться в главное меню']]));
   } catch (error) {
     await handleError(ctx, error, 'Ошибка при обработке выбора "Клиент EMEX.RU"');
   }
@@ -153,7 +228,10 @@ bot.hears(['Клиент EMEX.RU'], async (ctx) => {
 // Обработчик выбора "Хочу стать поставщиком"
 bot.hears(['Хочу стать поставщиком'], async (ctx) => {
   try {
-    ctx.reply([messages.supplier]);
+    const userId = ctx.from.id;
+
+    logUserAction(userId, 'Хочу стать поставщиком');
+    ctx.reply(messages.supplier, questionsKeyboard([['Вернуться в главное меню']]));
   } catch (error) {
     await handleError(ctx, error, 'Ошибка при обработке выбора "Хочу стать поставщиком"');
   }
@@ -162,9 +240,12 @@ bot.hears(['Хочу стать поставщиком'], async (ctx) => {
 // Как оформить заказ
 bot.hears('Как оформить заказ?', async (ctx) => {
   try {
+    const userId = ctx.from.id;
+
+    logUserAction(userId, 'Как оформить заказ?');
     const orderKeyboard = questionsKeyboard([
       ['Как пополнить баланс?'],
-      ['Остались вопросы', 'Вернуться в начало'],
+      ['Остались вопросы', 'Вернуться в главное меню'],
     ]);
 
     ctx.reply(messages.createOrder, orderKeyboard);
@@ -176,6 +257,9 @@ bot.hears('Как оформить заказ?', async (ctx) => {
 // Как пополнить баланс
 bot.hears('Как пополнить баланс?', async (ctx) => {
   try {
+    const userId = ctx.from.id;
+
+    logUserAction(userId, 'Как пополнить баланс?');
     const balanceImages = [
       path.join('images', 'balance-1.png'),
       path.join('images', 'balance-2.jpg'),
@@ -196,6 +280,9 @@ bot.hears('Как пополнить баланс?', async (ctx) => {
 // Как узнать ПВЗ
 bot.hears('Как узнать какой выбран ПВЗ?', async (ctx) => {
   try {
+    const userId = ctx.from.id;
+
+    logUserAction(userId, 'Как узнать какой выбран ПВЗ?');
     const pvzImages = [
       path.join('images', 'pvz-1.jpg'),
       path.join('images', 'pvz-2.jpg'),
@@ -217,6 +304,9 @@ bot.hears('Как узнать какой выбран ПВЗ?', async (ctx) => 
 // Как узнать статус заказа
 bot.hears('Как узнать текущий статус заказа?', async (ctx) => {
   try {
+    const userId = ctx.from.id;
+
+    logUserAction(userId, 'Как узнать текущий статус заказа?');
     ctx.reply(messages.statusInfo, ussualKeyboard());
   } catch (error) {
     await handleError(
@@ -230,6 +320,9 @@ bot.hears('Как узнать текущий статус заказа?', async
 // Есть ли деталь в наличии
 bot.hears('Есть ли запчасть в наличии?', async (ctx) => {
   try {
+    const userId = ctx.from.id;
+
+    logUserAction(userId, 'Есть ли запчасть в наличии?');
     ctx.reply(messages.stockInfo, ussualKeyboard());
   } catch (error) {
     await handleError(
@@ -243,7 +336,10 @@ bot.hears('Есть ли запчасть в наличии?', async (ctx) => {
 // Как оформить возврат
 bot.hears('Как оформить возврат запчасти?', async (ctx) => {
   try {
-    ctx.reply(messages.returnInfo);
+    const userId = ctx.from.id;
+
+    logUserAction(userId, 'Как оформить возврат запчасти?');
+    ctx.reply(messages.returnInfo, questionsKeyboard([['Вернуться в главное меню']]));
   } catch (error) {
     await handleError(
       ctx,
@@ -253,18 +349,28 @@ bot.hears('Как оформить возврат запчасти?', async (ctx
   }
 });
 
-// Обработчик "Вернуться в начало"
-bot.hears(['Вернуться в начало'], async (ctx) => {
+// Обработчик "Вернуться в главное меню"
+bot.hears(['Вернуться в главное меню'], async (ctx) => {
   try {
+    const userId = ctx.from.id;
+
+    logUserAction(userId, 'Вернуться в главное меню');
     ctx.reply(messages.customerType, customerKeyboard());
   } catch (error) {
-    await handleError(ctx, error, 'Ошибка при обработке выбора "Вернуться в начало"');
+    await handleError(
+      ctx,
+      error,
+      'Ошибка при обработке выбора "Вернуться в главное меню"',
+    );
   }
 });
 
 // Обработчик "Остались вопросы"
 bot.hears(['Остались вопросы', 'Вопроса нет в предложенных'], async (ctx) => {
   try {
+    const userId = ctx.from.id;
+
+    logUserAction(userId, 'Остались вопросы, Вопроса нет в предложенных');
     ctx.reply(messages.noAnswerInfo);
   } catch (error) {
     await handleError(
