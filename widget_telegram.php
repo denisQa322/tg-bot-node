7272a15134a6ae52ecd82ef6434f3a9d3834c273
denisQa322 <denis.isakov.99@gmail.com>
2024-12-12 10:11:54 +0500

    try with initial PHP

--- widget_telegram.php
+++ widget_telegram.php
@@ -1,31 +1,36 @@
-<?php
+<?
 function CheckWidgetAction($data)
 {
-    if (empty($data['message']['text']) && empty($data['message']['photo']) && empty($data['message']['video'])) {
+    if(empty($data['message']['text']))
+    {
         return false;
     }
+    $text = $data['message']['text'];
 
-    $ch = curl_init('https://telegramwh.omnidesk.ru/webhooks/telegram/7037/de6bf551b7e2170e');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    if($text != '/start')
+    {
+        $ch = curl_init('https://telegramwh.omnidesk.ru/webhooks/telegram/7037/de6bf551b7e2170e');
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0 );
+        curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
 
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 
-    $result = curl_exec($ch);
-    $result = !empty($result) ? @json_decode($result, TRUE) : null;
+        $result = curl_exec($ch);
+        $result = !empty($result) ? @json_decode($result,TRUE) : null;
 
-    curl_close($ch);
-    if (isset($result['success']) && $result['success'] === '2') {
-        return false;
-    } elseif ($result['success']) {
-        return true;
+        curl_close ($ch);
+        if(isset($result['success']) && $result['success'] === '2' ) {
+            return false;
+        }
+        elseif($result['success']) {
+            return true;
+        }
     }
-    return false;
+    return FALSE;
 }
-if (CheckWidgetAction(json_decode(file_get_contents("php://input"), TRUE))) {
-    die;
-}
-
-
+   if(CheckWidgetAction(json_decode(file_get_contents("php://input"),TRUE)))
+   {
+       die;
+   }
 ?>
