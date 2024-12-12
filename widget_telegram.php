b80f7c06fb9f6d592841c1c504b97a9472390650
denisQa322 <denis.isakov.99@gmail.com>
2024-12-12 17:55:58 +0500

    omnidesk link fix

--- widget_telegram.php
+++ widget_telegram.php
@@ -1,42 +1,36 @@
-<?php
+<?
 function CheckWidgetAction($data)
 {
-    // Если в данных есть поле bot_reply, логируем его
-    if (!empty($data['bot_reply'])) {
-        file_put_contents('bot_replies.log', date('Y-m-d H:i:s') . " - Ответ бота: " . $data['bot_reply'] . PHP_EOL, FILE_APPEND);
-        return true; // Можно вернуть true, так как это не ошибка
-    }
-
-    // Проверяем текст сообщения
-    if (empty($data['message']['text'])) {
+    if(empty($data['message']['text']))
+    {
         return false;
     }
     $text = $data['message']['text'];
 
-    // Если сообщение не /start, отправляем его в Omnidesk
-    if ($text != '/start') {
-        $ch = curl_init('https://telegramwh.omnidesk.ru/webhooks/telegram/7037/de6bf551b7e2170e');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    if($text != '/start')
+    {
+        $ch = curl_init('https://telegramwh.omnidesk.ru/webhooks/telegram/7042/8fcf89793acb6780');
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0 );
+        curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
 
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 
         $result = curl_exec($ch);
-        $result = !empty($result) ? @json_decode($result, TRUE) : null;
+        $result = !empty($result) ? @json_decode($result,TRUE) : null;
 
-        curl_close($ch);
-        if (isset($result['success']) && $result['success'] === '2') {
+        curl_close ($ch);
+        if(isset($result['success']) && $result['success'] === '2' ) {
             return false;
-        } elseif ($result['success']) {
+        }
+        elseif($result['success']) {
             return true;
         }
     }
-    return false;
+    return FALSE;
 }
-
-// Обработка входящих данных
-if (CheckWidgetAction(json_decode(file_get_contents("php://input"), TRUE))) {
-    die;
-}
+   if(CheckWidgetAction(json_decode(file_get_contents("php://input"),TRUE)))
+   {
+       die;
+   }
 ?>
